975b07b7240442f85e66dae8c5eba0d972c64c69
Thomas Kalhøj Clemensen <tkc@trifork.com>
2018-09-06 07:12:57 +0200

    Added LAContext extensions and bumped version to 1.0.6.

---
+++ TriforkSwiftExtensions/TriforkSwiftExtensions/LAContext/LAContextExtension.swift
@@ -0,0 +1,55 @@
+//
+//  LAContextExtension.swift
+//  TriforkSwiftExtensions
+//
+//  Created by Thomas Kalhøj Clemensen on 06/09/2018.
+//  Copyright © 2018 Trifork A/S. All rights reserved.
+//
+
+import Foundation
+import LocalAuthentication
+
+extension LAContext {
+    enum BiometricType: String {
+        case none
+        case touchID
+        case faceID
+        
+        func name() -> String {
+            let title: String
+            
+            switch self {
+            case .touchID:
+                title = "TouchID"
+            case .faceID:
+                title = "FaceID"
+            default:
+                title = ""
+            }
+            
+            return title
+        }
+    }
+    
+    static var biometricType: BiometricType {
+        var error: NSError?
+        let context: LAContext = LAContext()
+        
+        guard context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) else {
+            return .none
+        }
+        
+        if #available(iOS 11.0, *) {
+            switch context.biometryType {
+            case .none:
+                return .none
+            case .touchID:
+                return .touchID
+            case .faceID:
+                return .faceID
+            }
+        } else {
+            return  context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: nil) ? .touchID : .none
+        }
+    }
+}
