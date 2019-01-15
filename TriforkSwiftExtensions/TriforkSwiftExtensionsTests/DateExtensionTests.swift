3e28e113ee195cfd913ad03539d15b394b4eb8f4
Kim de Vos <kdv@trifork.com>
2019-01-15 17:44:12 +0100

    remove unused code

--- TriforkSwiftExtensions/TriforkSwiftExtensionsTests/DateExtensionTests.swift
+++ TriforkSwiftExtensions/TriforkSwiftExtensionsTests/DateExtensionTests.swift
@@ -11,12 +11,7 @@ import XCTest
 class DateExtensionTests: XCTestCase {
     
     let date: Date = Date(timeIntervalSince1970: 1503924052)
-    
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
+        
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
