4f566f92d920962a4beae4e653d763bb91f73b7d
Kim de Vos <kdv@trifork.com>
2019-02-14 16:05:07 +0100

    Changed trim -> trimming() and slice -> sliced()

--- TriforkSwiftExtensions/TriforkSwiftExtensions/String/StringProtocolExtension.swift
+++ TriforkSwiftExtensions/TriforkSwiftExtensions/String/StringProtocolExtension.swift
@@ -9,19 +9,19 @@
 import Foundation
 
 public extension StringProtocol where Self.Index == String.Index {
+
     /// Returns a trimmmed string
-    public var trim: String {
+    public func trimmed() -> String {
         return self.trimmingCharacters(in: .whitespacesAndNewlines)
     }
 
-    /// /// Returns a sliced string that is inbetween from and to.
+    /// Returns a new sliced string that is inbetween from and to.
     ///
     /// - Parameters:
     ///   - from: The string that the range should start at
     ///   - to: The string the range should end at
     /// - Returns: If there is a valid range
-    public func slice(from: Self, to: String) -> String? {
-
+    public func slicing(from: Self, to: String) -> String? {
         guard let startRange = range(of: from)?.upperBound,
             let endRange = range(of: to, range: startRange..<endIndex)?.lowerBound else { return nil }
         return String(self[startRange..<endRange])
--- TriforkSwiftExtensions/TriforkSwiftExtensionsTests/StringProtocolTests.swift
+++ TriforkSwiftExtensions/TriforkSwiftExtensionsTests/StringProtocolTests.swift
@@ -11,61 +11,55 @@ import XCTest
 class StringProtocolTests: XCTestCase {
 
     func testStringTrimWithWhitespaceAndNewLine() {
-        let testString = "\nHello World  \n".trim
+        let testString = "\nHello World  \n".trimmed()
 
         XCTAssertEqual(testString, "Hello World")
     }
 
     func testStringTrimmedString() {
-        let testString = "Hello World".trim
+        let testString = "Hello World".trimmed()
 
         XCTAssertEqual(testString, "Hello World")
     }
 
     func testStringWhitespaceString() {
-        let testString = "   Hello World   ".trim
+        let testString = "   Hello World   ".trimmed()
 
         XCTAssertEqual(testString, "Hello World")
     }
 
     func testSubstringTrimWithWhitespaceAndNewLine() {
-        let testString = Substring("\nHello World  \n").trim
+        let testString = Substring("\nHello World  \n").trimmed()
 
         XCTAssertEqual(testString, "Hello World")
     }
 
     func testSubstringTrimmedString() {
-        let testString = Substring("Hello World").trim
+        let testString = Substring("Hello World").trimmed()
 
         XCTAssertEqual(testString, "Hello World")
     }
 
     func testSubstringWhitespaceString() {
-        let testString = Substring("   Hello World   ").trim
+        let testString = Substring("   Hello World   ").trimmed()
 
         XCTAssertEqual(testString, "Hello World")
     }
 
     func testStringSlicing() {
-        let testString = "App=Foo&App=Bar".slice(from: "=", to: "&")
+        let testString = "App=Foo&App=Bar".slicing(from: "=", to: "&")
 
         XCTAssertEqual(testString, "Foo")
     }
 
     func testStringSlicingWithInvalidTo() {
-        let testString = "App=Foo&App=Bar".slice(from: "=", to: "!")
+        let testString = "App=Foo&App=Bar".slicing(from: "=", to: "!")
 
         XCTAssertNil(testString)
     }
 
     func testStringSlicingWithInvalidFrom() {
-        let testString = "App=Foo&App=Bar".slice(from: "!", to: "&")
-
-        XCTAssertNil(testString)
-    }
-
-    func testStringSlicingWithInvalidFromAndTo() {
-        let testString = "App=Foo&App=Bar".slice(from: "!", to: "!")
+        let testString = "App=Foo&App=Bar".slicing(from: "!", to: "&")
 
         XCTAssertNil(testString)
     }
