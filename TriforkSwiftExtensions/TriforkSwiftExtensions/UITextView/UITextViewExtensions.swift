d7d1a4099044ff84ed1fdd2458e7f84bb4112ecb
Kim de Vos <kdv@trifork.com>
2019-04-26 09:38:59 +0200

    Renamed mehtod from setZeroInset -> setTextContainerInsetToZero

--- TriforkSwiftExtensions/TriforkSwiftExtensions/UITextView/UITextViewExtensions.swift
+++ TriforkSwiftExtensions/TriforkSwiftExtensions/UITextView/UITextViewExtensions.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 extension UITextView {
-    func setZeroInset() {
+    func setTextContainerInsetToZero() {
         textContainerInset = .zero
         textContainer.lineFragmentPadding = 0
     }
