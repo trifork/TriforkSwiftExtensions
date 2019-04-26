48e736314158d634498189fb38da10aa8601d22d
Kim de Vos <kdv@trifork.com>
2019-04-26 09:30:36 +0200

    Added setZetInsets to UITextView

---
+++ TriforkSwiftExtensions/TriforkSwiftExtensions/UITextView/UITextViewExtensions.swift
@@ -0,0 +1,16 @@
+//
+//  UITextViewExtensions.swift
+//  TriforkSwiftExtensions
+//
+//  Created by Kim de Vos on 26/04/2019.
+//  Copyright © 2019 Trifork A/S. All rights reserved.
+//
+
+import UIKit
+
+extension UITextView {
+    func setZeroInset() {
+        textContainerInset = .zero
+        textContainer.lineFragmentPadding = 0
+    }
+}
