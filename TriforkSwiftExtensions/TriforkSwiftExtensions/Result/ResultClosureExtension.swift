e95f0f01773dc4206d7520ae788f961ef3055ad9
Thomas Kalhøj Clemensen <tkc@trifork.com>
2019-05-02 13:16:29 +0200

    Result extension with success and failure closure handlers (inspired by BrightFutures/Result).

---
+++ TriforkSwiftExtensions/TriforkSwiftExtensions/Result/ResultClosureExtension.swift
@@ -0,0 +1,34 @@
+//
+//  ResultClosureExtension.swift
+//  TriforkSwiftExtensions
+//
+//  Created by Thomas Kalhøj Clemensen on 02/05/2019.
+//  Copyright © 2019 Trifork A/S. All rights reserved.
+//
+
+import Foundation
+
+public extension Result {
+    
+    /// `handler` is only called upon success, with the value as parameter.
+    ///
+    /// Returns the receiver, to allow further operations, e.g. `failure(_:)`
+    @discardableResult
+    func success(_ handler: ((Success) -> Void)) -> Result<Success, Failure> {
+        if case let .success(value) = self {
+            handler(value)
+        }
+        return self
+    }
+    
+    /// `handler` is only called upon failure, with the error as parameter.
+    ///
+    /// Returns the receiver, to allow further operations, e.g. `success(_:)`
+    @discardableResult
+    func failure(_ handler: ((Failure) -> Void)) -> Result<Success, Failure> {
+        if case let .failure(error) = self {
+            handler(error)
+        }
+        return self
+    }
+}
