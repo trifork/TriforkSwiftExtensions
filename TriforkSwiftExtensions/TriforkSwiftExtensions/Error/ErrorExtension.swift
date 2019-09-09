f42a0b64e3de1de9a6c94ecd2e631a830302c44a
Kim de Vos <kdv@trifork.com>
2019-09-09 09:33:24 +0200

    Added error extension for network errors

---
+++ TriforkSwiftExtensions/TriforkSwiftExtensions/Error/ErrorExtension.swift
@@ -0,0 +1,23 @@
+//
+//  ErrorExtension.swift
+//  TriforkSwiftExtensions
+//
+//  Created by Kim de Vos on 09/09/2019.
+//  Copyright © 2019 Trifork A/S. All rights reserved.
+//
+
+import Foundation
+
+extension Error {
+    public var isNetworkError: Bool {
+        let error = self as NSError
+        let networkErrors = [NSURLErrorNetworkConnectionLost,
+                             NSURLErrorNotConnectedToInternet,
+                             NSURLErrorCannotLoadFromNetwork,
+                             NSURLErrorSecureConnectionFailed,
+                             NSURLErrorInternationalRoamingOff,
+                             NSURLErrorTimedOut]
+
+        return error.domain == NSURLErrorDomain && networkErrors.contains(error.code)
+    }
+}
