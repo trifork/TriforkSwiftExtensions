f99e24e5579992177cdf8fcd3455b28f8c06bf15
Thomas Kalhøj Clemensen <tkc@trifork.com>
2020-12-09 08:16:15 +0100

    Added new keypath methods on sequence.

--- TriforkSwiftExtensions/TriforkSwiftExtensions/Sequence/SequenceExtensions.swift
+++ TriforkSwiftExtensions/TriforkSwiftExtensions/Sequence/SequenceExtensions.swift
@@ -16,3 +16,27 @@ public extension Sequence where Element: Cancellable {
     func cancel() { forEach { $0.cancel() } }
 }
 #endif
+
+extension Sequence {
+
+    /// Sorts list by key path
+    func sorted<T: Comparable>(by keyPath: KeyPath<Element, T>, using comparator: (T, T) -> Bool = (<)) -> [Element] {
+        sorted { a, b in
+            comparator(a[keyPath: keyPath], b[keyPath: keyPath])
+        }
+    }
+
+    /// max function by key path
+    func max<T: Comparable>(by keyPath: KeyPath<Element, T>, using comparator: (T, T) -> Bool = (<)) -> Element? {
+        self.max { a, b in
+            comparator(a[keyPath: keyPath], b[keyPath: keyPath])
+        }
+    }
+
+    /// min function by key path
+    func min<T: Comparable>(by keyPath: KeyPath<Element, T>, using comparator: (T, T) -> Bool = (<)) -> Element? {
+        self.min { a, b in
+            comparator(a[keyPath: keyPath], b[keyPath: keyPath])
+        }
+    }
+}
