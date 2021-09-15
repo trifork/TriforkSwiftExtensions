317317ac3ae62fc724c2e8195aef260c412863cb
Christian Alsaybar Mortensen <cam@dawnhealth.com>
2021-09-15 09:58:08 +0200

    Set minimum iOS version to 13

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "TriforkSwiftExtensions",
     platforms: [
-       .iOS(.v9),
+       .iOS(.v13),
     ],
     products: [
         .library(name: "TriforkSwiftExtensions",
